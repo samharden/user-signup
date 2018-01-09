2c8aa2e91893dd10b72ee8586a75d5032f1187af
samharden <samharden@gmail.com>
2018-01-09 16:02:24 -0500

    More fixes to the logic

--- main.py
+++ main.py
@@ -28,22 +28,27 @@ def log_in():
     un_error = ''
     pw_result = ''
     email_result = ''
-    if len(pw) > 4 and pw == pw2 and '@' in em and len(un) > 3 and len(un)<20:
+    if len(pw) > 4 and pw == pw2 and len(em) == 0 and len(un) > 3 and len(un)<20:
         return render_template('success.html', text = un)
-    elif pw == pw2 and '@' in em and len(un) < 3:
+    elif pw == pw2 and len(un) < 3:
         un_error = 'Username must be at least 4 characters'
-    elif pw != pw2 and '@' not in em:
+    elif pw != pw2 and len(em) > 0 and '@' not in em:
         pw_result = 'Passwords do not match'
         email_result = 'Not a valid email address!'
-    elif '@' not in em:
+    elif pw != pw2 and len(pw) <= 4:
+        pw_result = 'Passwords do not match and are too short!'
+    elif pw != pw2:
+        pw_result = 'Passwords do not match'
+    elif len(pw) <= 4:
+        pw_result = 'Password must be at least 5 characters!'
+    elif len(em) > 0 and '@' not in em:
         email_result = 'Not a valid email address!'
-        pw_result = ''
-    elif '@' in em and pw == pw2 and len(un)>3:
-        return redirect('success.html')
 
 
     return render_template(
                             'home.html',
+                            un = un,
+                            email= em,
                             name_error = un_error,
                             password1_error = pw_result,
                             password2_error = pw_result,
